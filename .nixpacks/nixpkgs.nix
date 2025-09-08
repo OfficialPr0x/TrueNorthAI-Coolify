e10b5ec1bae71a7a491e2c84c51c7bd7a6e8bbae
OfficialPr0x <jbeam4825@gmail.com>
2025-09-08 13:57:57 -0400

    Add Nixpacks configuration file to define package dependencies including Node.js, npm, Python, and build tools

---
+++ .nixpacks/nixpkgs.nix
@@ -0,0 +1,15 @@
+let
+  pkgs = import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/5c37ad87222cfc1ec36d6cd1364514a9efc2f7f2.tar.gz") {
+    overlays = [
+      (import (fetchTarball "https://github.com/railwayapp/nix-npm-overlay/archive/main.tar.gz"))
+    ];
+  };
+in
+with pkgs; [
+  nodejs-20_x
+  npm-9_x
+  python3
+  gcc
+  make
+  pkg-config
+]
